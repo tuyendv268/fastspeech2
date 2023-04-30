b48111798703c08c8e0103b1b6c078713d545c57
tuyendv <tuyen.duongvan268@gmail.com>
2023-05-01 00:06:17 +0700

    update distributed training

--- model/fastspeech2.py
+++ model/fastspeech2.py
@@ -57,10 +57,9 @@ class FastSpeech2(nn.Module):
         e_control=1.0,
         d_control=1.0,
     ):
-        print("\nDevice", src_lens, max_src_len, device)
         src_masks = get_mask_from_lengths(lengths=src_lens, max_len=max_src_len, device=device)
         mel_masks = (
-            get_mask_from_lengths(mel_lens, max_mel_len)
+            get_mask_from_lengths(mel_lens, max_mel_len, device=device)
             if mel_lens is not None
             else None
         )
